74db70a2f9325873c0fa7751f9d6709e1010cbf5
samirm00 <sammoham.be@gmail.com>
2021-05-04 14:46:41 +0200

    hello world

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,24 @@
-import React, { Component } from 'react';
-import { Link, Route, Switch } from 'react-router-dom';
-import withAuth from './withAuth';
-import Home from './Home';
-import Secret from './Secret';
-import Login from './Login';
+import React, { Component } from "react";
+import { Link, Route, Switch } from "react-router-dom";
+import withAuth from "./withAuth";
+import Home from "./Home";
+import Secret from "./Secret";
+import Login from "./Login";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/secret">Secret</Link></li>
-          <li><Link to="/login">Login</Link></li>
+      <div className="container">
+        <ul className="list">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/secret">Secret</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
         </ul>
 
         <Switch>
--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,24 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class Home extends Component {
   constructor() {
     super();
     this.state = {
-      message: 'Loading...'
-    }
+      message: "Loading...",
+    };
   }
-  
+
   componentDidMount() {
-    fetch('/api/home')
-      .then(res => res.text())
-      .then(res => this.setState({message: res}));
+    fetch("/api/home")
+      .then((res) => res.text())
+      .then((res) => this.setState({ message: res }));
   }
-  
+
   render() {
     return (
-      <div>
+      <div className="home">
         <h1>Home</h1>
-        <p>{this.state.message}</p>
+        <p className="par">{this.state.message}</p>
       </div>
     );
   }
