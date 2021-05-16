f071abb7720fa2cc22a4357865ac55398aff8fc1
samirm00 <sammoham.be@gmail.com>
2021-05-16 18:20:57 +0200

    solve mearging error

--- api/models/GoodsForMany.js
+++ api/models/GoodsForMany.js
@@ -1,54 +1,44 @@
-const { Sequelize, Model, DataTypes, STRING } = require("sequelize");
+const { Sequelize } = require("sequelize");
 const sequelize = require("../db/db.js");
 const User = require("./User.js");
 const Goods = require("./Goods.js");
 
-class GoodsForMany extends Model {
-  constructor({ goodformany_id, goods_id, needer_id }) {
-    super();
+const GoodsForMany = sequelize.define(
+    "goodsformany",
+    {
+        goodformany_id: {
+            type: Sequelize.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        goods_id: {
+            type: Sequelize.INTEGER,
+            allowNull: false,
+        },
+        needer_id: {
+            type: Sequelize.INTEGER,
+            allowNull: false,
+        },
 
-    this.goodformany_id = goodformany_id;
-    this.goods_id = goods_id;
-    this.needer_id = needer_id;
-  }
-}
-
-GoodsForMany.init(
-  {
-    goodformany_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
+        createdAt: Sequelize.DATE,
+        updatedAt: Sequelize.DATE,
     },
-    goods_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    needer_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-
-    createdAt: Sequelize.DATE,
-    updatedAt: Sequelize.DATE,
-  },
-  {
-    sequelize,
-    modelName: "tags",
-  }
+    {
+        tableName: "goodsformany",
+    }
 );
 
 // foreign keys
 
 Goods.hasOne(GoodsForMany, {
-  as: "goodsformany",
-  foreignKey: "goods_id",
+    as: "goodsformany",
+    foreignKey: "goods_id",
 });
 
 GoodsForMany.belongsTo(User, {
-  as: "user",
-  foreignKey: "user_id",
+    as: "user",
+    foreignKey: "user_id",
 });
 
 module.exports = GoodsForMany;
--- api/models/User.js
+++ api/models/User.js
@@ -1,134 +1,96 @@
 const { cryptPassword } = require("../utils/encryption");
-const { Sequelize, Model, DataTypes, STRING } = require("sequelize");
+const { Sequelize } = require("sequelize");
 const sequelize = require("../db/db.js");
 const bcrypt = require("bcrypt");
 
-class User extends Model {
-  constructor({
-    user_id,
-    first_name,
-    last_name,
-    email,
-    password,
-    street,
-    phone,
-    age,
-    is_giver,
-    is_needer,
-    description,
-    agreement,
-  }) {
-    super();
-    this.user_id = user_id;
-    this.first_name = first_name;
-    this.last_name = last_name;
-    this.email = email;
-    this.password = password;
-    this.street = street;
-    this.phone = phone;
-    this.age = age;
-    this.is_giver = is_giver;
-    this.is_needer = is_needer;
-    this.description = description;
-    this.agreement = agreement;
-  }
+const User = sequelize.define(
+    "user",
+    {
+        user_id: {
+            type: Sequelize.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        first_name: {
+            type: Sequelize.STRING(50),
+            allowNull: false,
+        },
+        last_name: {
+            type: Sequelize.STRING(50),
+            allowNull: false,
+        },
+        email: {
+            type: Sequelize.STRING(50),
+            unique: true,
+            validate: {
+                isEmail: true,
+            },
+        },
+        street: {
+            type: Sequelize.STRING(255),
+            allowNull: true,
+        },
+        password: {
+            type: Sequelize.STRING(255),
+            allowNull: false,
+        },
+        phone: {
+            type: Sequelize.STRING(50),
+            allowNull: true,
+        },
+        age: {
+            type: Sequelize.INTEGER,
+            validate: {
+                max: 120,
+                min: 18,
+            },
+        },
+        is_needer: {
+            type: Sequelize.BOOLEAN,
+            max: 1,
+            min: 0,
+            allowNull: false,
+        },
+        is_giver: {
+            type: Sequelize.BOOLEAN,
+            max: 1,
+            min: 0,
+            allowNull: false,
+        },
+        description: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+            },
+        },
+        agreement: {
+            type: Sequelize.BOOLEAN,
+            allowNull: false,
+            validate: {
+                max: 1,
+                min: 1,
+            },
+        },
 
-  validPassword(password) {
-    return bcrypt.compare(password, this.password);
-  }
-}
-
-User.init(
-  {
-    user_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
+        // time stamp
+        createdAt: Sequelize.DATE,
+        updatedAt: Sequelize.DATE,
     },
-    first_name: {
-      type: DataTypes.STRING(50),
-      allowNull: false,
-    },
-    last_name: {
-      type: DataTypes.STRING(50),
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING(50),
-      unique: true,
-      validate: {
-        isEmail: true,
-      },
-    },
-    street: {
-      type: DataTypes.STRING(255),
-      allowNull: true,
-    },
-    password: {
-      type: DataTypes.STRING(255),
-      allowNull: false,
-    },
-    phone: {
-      type: DataTypes.STRING(50),
-      allowNull: true,
-    },
-    age: {
-      type: DataTypes.INTEGER,
-      validate: {
-        max: 120,
-        min: 18,
-      },
-    },
-    is_needer: {
-      type: DataTypes.BOOLEAN,
-      max: 1,
-      min: 0,
-      allowNull: false,
-    },
-    is_giver: {
-      type: DataTypes.BOOLEAN,
-      max: 1,
-      min: 0,
-      allowNull: false,
-    },
-    description: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notEmpty: true,
-      },
-    },
-    agreement: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      validate: {
-        max: 1,
-        min: 1,
-      },
-    },
-  },
-  {
-    sequelize,
-    modelName: "user",
-  }
+    {
+        tableName: "users",
+    }
 );
 
 User.beforeCreate((user, options) => {
-  return cryptPassword(user.password)
-    .then((success) => {
-      user.password = success;
-    })
-    .catch((err) => {
-      if (err) console.log(err);
-    });
+    return cryptPassword(user.password)
+        .then((success) => {
+            user.password = success;
+        })
+        .catch((err) => {
+            if (err) console.log(err);
+        });
 });
 
 module.exports = User;
-
-/**
- * tests
- * ------
- * 1- agreement must be 1
- * 2- description allows empty string should be changed || solve bu adding unique
- */
