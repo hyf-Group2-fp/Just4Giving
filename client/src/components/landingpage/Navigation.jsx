1c31dac0ce03a04bfb75ae60ba36d9f91ff82dc1
peyman <pshmd7@gmail.com>
2021-05-16 10:29:47 +0200

    about us pop up

--- client/src/components/landingpage/Navigation.jsx
+++ client/src/components/landingpage/Navigation.jsx
@@ -1,7 +1,9 @@
-import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
+import React, { useState } from "react";
+import { Navbar, Nav, Modal } from "react-bootstrap";
 import logo from "../../assets/landingpage/logo22.png";
 function Navigation() {
+  const [show, setShow] = useState(false);
+  const handleShowAboutUs = () => setShow(true);
   return (
     <div className="margin-t-b">
       <Navbar collapseOnSelect expand="lg" sticky="top">
@@ -21,7 +23,7 @@ function Navigation() {
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="row justify-content-end links">
             <Nav.Item>
-              <Nav.Link href="/aboutus">About Us</Nav.Link>
+              <Nav.Link onClick={handleShowAboutUs}>About Us</Nav.Link>
             </Nav.Item>
             <Nav.Item>
               <Nav.Link href="/login">Login</Nav.Link>
@@ -29,6 +31,39 @@ function Navigation() {
           </Nav>
         </Navbar.Collapse>
       </Navbar>
+      <Modal
+        size={"xl"}
+        show={show}
+        onHide={() => setShow(false)}
+        dialogClassName="modal-90w"
+        aria-labelledby="example-custom-modal-styling-title"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="example-custom-modal-styling-title">
+            <h1>About Us</h1>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h3>Every Kindness Counts </h3>
+          <p>
+            Just4giving is more than just a list of goods. It’s a place of one’s
+            dare to care, to transform the lives of unprivileged people in the
+            society.
+          </p>
+          <hr />
+          <h3>Vision</h3>
+          <p>Every human being have rights and deserve to have a good life. </p>
+          <hr />
+          <h3>Mission</h3>
+          <p>
+            Just4giving is a platform that welcomes underprivileged individuals
+            or new comers with the aim of offering them a way to support them in
+            positively material growing. Just4giving is also a place for good
+            people who wants to share they stuff with others. While doing so,
+            providing them a place to connected is our pleasure.
+          </p>
+        </Modal.Body>
+      </Modal>
     </div>
   );
 }
