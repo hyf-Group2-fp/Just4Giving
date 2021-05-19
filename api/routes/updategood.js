59342d255a8da96ace4c26eaa8647b98860a5427
Ketunze Brain <brainketunze@gmail.com>
2021-05-19 08:57:55 +0200

    update goods

--- api/routes/updategood.js
+++ api/routes/updategood.js
@@ -4,29 +4,32 @@ const Goods = require("../models/Goods.js");
 // initialize express
 const app = express();
 
-app.put("/updategood", (req, res) => {
-  const id =req.body.id;
-  const good = req.body.good;
-
-  db.query("UPDATE category SET good = ? WHERE id = ?",
-  [good, id],
-  (err, result) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.send (result);
-    }
+app.put("/goods/:id", async (req, res) => {
+  try {
+    res.send(`good id put ${req.params.id}`);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("server error");
   }
- );
-})
-// app.put("/goods/:id", async (req, res) => {
-//   try {
-//     res.send(`good id put ${req.params.id}`);
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).send("server error");
+});
+
+
+
+// app.put("/goods", (req, res) => {
+//   const id =req.body.id;
+//   const good = req.body.good;
+
+//   db.query("UPDATE category SET good = ? WHERE id = ?",
+//   [good, id],
+//   (err, result) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       res.send (result);
+//     }
 //   }
-// });
+//  );
+// })
 
 module.exports = app;
 
