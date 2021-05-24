5da558bdd8224657420deb1ccdd63bd6f0e728e3
Bermarte <bernardo.martelli@gmail.com>
2021-05-25 00:47:57 +0200

    categories divs and titles

--- client/src/components/categories/categories.jsx
+++ client/src/components/categories/categories.jsx
@@ -1,13 +1,20 @@
-import React, { useEffect, useState } from "react";
-
-
+import React, {useEffect, useState} from "react";
+import axios from "axios";
 
 function Categories() {
     const [isOpen, setOpen] = useState(false);
+    const [categories, setCategories] = useState(null);
+    const url = `http://localhost:5000/api/categories` ;
+    //fetch the categories
+    const FetchCategories = async () => {
+      const response = await axios.get(url);
+      const resCategories = response.data;
+      setCategories(resCategories);
+    } ;
 
     useEffect(() => {
-      const toggleBtn = document.getElementById("open-close-btn");
-      document
+        const toggleBtn = document.getElementById("open-close-btn");
+        document
             .getElementById("categories-panel")
             .style
             .left = "-256px";
@@ -15,19 +22,25 @@ function Categories() {
             .getElementById("main")
             .style
             .marginLeft = "0";
-            toggleBtn.classList.add("openbtn");
-            toggleBtn.classList.remove("closebtn");
+        toggleBtn
+            .classList
+            .add("openbtn");
+        toggleBtn
+            .classList
+            .remove("closebtn");
+
+        FetchCategories();
+
     }, []);
 
-    
     const handleToggle = () => {
-    
-      setOpen(!isOpen);
-      if (isOpen) {
-        return closeNav();
-      }else{
-        return openNav();
-      }
+
+        setOpen(!isOpen);
+        if (isOpen) {
+            return closeNav();
+        } else {
+            return openNav();
+        }
     };
 
     const openNav = () => {
@@ -42,12 +55,16 @@ function Categories() {
             .getElementById("main")
             .style
             .marginLeft = "250px";
-            toggleBtn.classList.add("closebtn");
-            toggleBtn.classList.remove("openbtn");
+        toggleBtn
+            .classList
+            .add("closebtn");
+        toggleBtn
+            .classList
+            .remove("openbtn");
     }
 
     const closeNav = () => {
-      const toggleBtn = document.getElementById("open-close-btn");
+        const toggleBtn = document.getElementById("open-close-btn");
 
         document
             .getElementById("categories-panel")
@@ -57,25 +74,39 @@ function Categories() {
             .getElementById("main")
             .style
             .marginLeft = "0";
-            toggleBtn.classList.add("openbtn");
-            toggleBtn.classList.remove("closebtn");
+        toggleBtn
+            .classList
+            .add("openbtn");
+        toggleBtn
+            .classList
+            .remove("closebtn");
     }
+
+    // console.log('test', categories);
+    // console.log('test here', categories[0].category_name)
     return (
         <div>
             <div id="categories-panel" className="sidebar">
                 <div id="categories-header">
                     <h2 id="categories-title-text">Select a category</h2>
                 </div>
-                <a href="#" id="open-close-btn" class="openbtn" onClick={() => handleToggle()}></a>
-                <div id="categories-body">
+                <a href="#" id="open-close-btn" className="openbtn" onClick={() => handleToggle()}></a>
+                <div id="categories-body" className="row-categories">
                     {/* categories */}
+                    {
+                      categories && 
+                      //console.log('test here', categories[0].category_name)
+                      categories.map(category => (
+                        <div className="category col-6"><div className="pt-3 category-title">{category.category_name}</div></div>
+                      ))
+                    } 
                 </div>
 
                 {/* categories */}
 
             </div>
             <div id="main">
-              {/* this div will be moved together with the panel,
+                {/* this div will be moved together with the panel,
               it can contains the cards */}
             </div>
         </div>
