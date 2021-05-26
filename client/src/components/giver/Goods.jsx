652de17b13163a45730cf136c7e076afc9ad7ce3
samirm00 <sammoham.be@gmail.com>
2021-05-26 15:04:44 +0200

    fix confilect issues with comopnents

--- client/src/components/giver/Goods.jsx
+++ client/src/components/giver/Goods.jsx
@@ -12,28 +12,64 @@ import team from '../../assets/landingpage/team.png';
 import { createGoods, updateGoods } from '../../redux/actions/goodsInfoAction';
 
 export default function Goods() {
-  const [goods, setGoods] = useState([]);
+  const [goods, setGoods] = useState({});
   const history = useHistory();
 
   // dispatch an action
   const dispatch = useDispatch();
+
   // access the state
   const user_id = useSelector((state) => state.userInfo.user_id);
-  const createdAt = useSelector((state) => state.goods.createdAt);
   console.log(user_id);
   const url = `http://localhost:5000/api/user/goods/${user_id}`;
+
   // fetch goods
   const FetchGoods = async () => {
     const response = await axios.get(url);
     const goods = response.data.goods;
+    console.log(goods);
+
     // fire an action
     dispatch(createGoods(goods));
-    console.log(response);
     setGoods(goods);
   };
+
   useEffect(() => {
     FetchGoods();
   }, [user_id]);
+
+  // details good
+  const detailGood = (id) => {
+    console.log(id);
+    history.push(`/detailsitem/${id}`);
+  };
+
+  // edit good
+
+  const editGood = (id) => {
+    console.log(id);
+    history.push(`/edititem/${id}`);
+  };
+
+  // delete good
+
+  const deleteGood = async (id, index) => {
+    const filterGoods = Object.entries(goods).filter(
+      (key, value) => key !== index
+    );
+    const newGoods = Object.fromEntries(filterGoods);
+    setGoods(newGoods);
+    // send a delete request
+
+    const response = await axios.delete(
+      `http://localhost:5000/api/goods/${id}`
+    );
+    console.log(response);
+    // refresh the page
+    history.push('/itemdelete');
+    history.push('/profilegiver');
+  };
+
   return (
     <div
       style={{
@@ -42,7 +78,7 @@ export default function Goods() {
         justifyContent: 'space-evenly',
       }}
     >
-      {goods.map((good, index) => (
+      {Object.values(goods).map((good, index) => (
         <Card
           className="itemCards"
           style={{ flexGrow: 1, width: '18rem' }}
@@ -62,13 +98,32 @@ export default function Goods() {
             <Card.Title>{good.item_name}</Card.Title>
 
             <Card.Text>{good.category}</Card.Text>
-            <Button
-              size={'sm'}
-              onClick={() => history.push(`/profilegiver/item/${index}`)}
-              variant="primary"
-            >
-              Details
-            </Button>
+            <div className="btn-good-group">
+              <Button
+                className="btn-good"
+                size={'sm'}
+                onClick={() => detailGood(good.goods_id)}
+                variant="secondary"
+              >
+                Details
+              </Button>
+              <Button
+                className="btn-good"
+                size={'sm'}
+                onClick={() => editGood(good.goods_id)}
+                variant="info"
+              >
+                Edit
+              </Button>
+              <Button
+                className="btn-good"
+                size={'sm'}
+                onClick={() => deleteGood(good.goods_id, index)}
+                variant="danger"
+              >
+                Delete
+              </Button>
+            </div>
           </Card.Body>
         </Card>
       ))}
