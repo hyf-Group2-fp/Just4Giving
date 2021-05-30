16c912b445e81fba30a1a9e2e8b516f22a1937a3
Anthony Meirlaen <anthony.meirlaen@gmail.com>
2021-05-30 13:42:59 +0200

    Send mail make sure error code is returned

--- api/routes/sendEmail.js
+++ api/routes/sendEmail.js
@@ -2,9 +2,9 @@ const express = require("express");
 const sgMail = require("@sendgrid/mail");
 const { SENDGRID_API_KEY } = require("../config");
 sgMail.setApiKey(SENDGRID_API_KEY);
-// initialize express
+
 const app = express();
-// send an email to the giver
+
 app.post("/good/sendEmail", async (req, res) => {
     const { to, from, subject, content } = req.body;
     const data = {
@@ -13,12 +13,12 @@ app.post("/good/sendEmail", async (req, res) => {
         subject: subject,
         html: content,
     };
-    console.log(data);
     try {
         await sgMail.send(data);
         res.status(200).send("the Email has been sent successfully!!");
     } catch (err) {
         console.error(err);
+        res.status(500).send("Something went wrong");
     }
 });
 module.exports = app;
